f7e4e1eabd3312bc2af1aca7272cd6b4eea3f4f5
Omobowale <stigawithfun@gmail.com>
2026-09-12 12:38:04 +0100

    Add landing page url

--- myblog/urls.py
+++ myblog/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('', auth_views.LoginView.as_view(template_name='login.html'), name="login"),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name="login"),
     path('logout/', auth_views.LogoutView.as_view(next_page='/auth/login/'), name="logout"),
     path('admin/', admin.site.urls),
